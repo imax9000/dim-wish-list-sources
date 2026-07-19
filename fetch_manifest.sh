8aabd663e44d12819f4c4079a2961c564f3272f7
Max Ignatenko <gelraen.ua@gmail.com>
2026-07-19 13:15:02 +0100

    change: tweak curl flags

--- fetch_manifest.sh
+++ fetch_manifest.sh
@@ -7,4 +7,4 @@ API_KEY=5ec01caf6aee450d9dabe646294ffdc9
 path="$(curl --silent --header 'X-API-Key: $(API_KEY)' https://www.bungie.net/Platform/Destiny2/Manifest/ \
   | jq --raw-output .Response.jsonWorldContentPaths.en)"
 url="https://www.bungie.net${path}"
-curl --silent --header 'X-API-Key: $(API_KEY)' --header 'Content-Type: application/json' "${url}" > ${1:-manifest.json}
+curl --verbose --fail --header 'X-API-Key: $(API_KEY)' --header 'Content-Type: application/json' "${url}" -o "${1:-manifest.json}"
